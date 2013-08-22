48fbe028a34c15cca67c888e8e83b34be98bf429
Jinggo Villamor <jingcleovil@gmail.com>
2013-08-22 22:55:38 +0800

    update themes

--- app/config/ragnarok.php
+++ app/config/ragnarok.php
@@ -116,7 +116,7 @@ return array(
 		),
 		'characters' => array(
 			"index"	=> AccountLevel::ADMIN,
-			"status" => AccountLevel::NORMAL
+			"stats" => AccountLevel::NORMAL
 		),
 		'cms' => array(
 			"index"	=> AccountLevel::ADMIN,
