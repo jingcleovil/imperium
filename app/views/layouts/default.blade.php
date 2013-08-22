7062fca9fe694846db5742eb249a84c486b6b37c
Jinggo Villamor <jingcleovil@gmail.com>
2013-08-22 19:38:18 +0800

    added auto height

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -3,6 +3,7 @@
 <html>
     <head>
       	<title>{{ $title }}</title>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
       	<meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
